66ca0ba945c585387d55e4a5a77c26211ba0d4e5
Michael Whiteside <m.whiteside@blueyonder.co.uk>
2022-02-04 12:29:19 +0000

    refactoring for Game1

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,81 +2,99 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
 
-        public TennisGame1(string player1Name, string player2Name)
-        {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
-        }
+        public int P1Score { get; set; }
+        public int P2Score { get; set; }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+                P1Score += 1;
             else
-                m_score2 += 1;
+                P2Score += 1;
         }
 
         public string GetScore()
         {
-            string score = "";
-            var tempScore = 0;
-            if (m_score1 == m_score2)
-            {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
-            }
-            else if (m_score1 >= 4 || m_score2 >= 4)
-            {
-                var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
-            }
-            else
-            {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
-            }
-            return score;
+            if (IsEqualScores()) return GetScoreStatusForEqualScores();
+            if (IsPerformingTieBreaker()) return GetScoreStatusWithinTieBreaker();
+            return GetGeneralCaseScoreStatus();
         }
+
+        private bool IsEqualScores()
+        {
+            return P1Score == P2Score;
+        }
+
+        private string GetGeneralCaseScoreStatus()
+        {
+            string scoreStatus = "";
+            scoreStatus += GetGeneralCaseScoreStatusForPlayer(P1Score);
+            scoreStatus += "-";
+            scoreStatus += GetGeneralCaseScoreStatusForPlayer(P2Score);
+            return scoreStatus;
+        }
+
+        private string GetGeneralCaseScoreStatusForPlayer(int score)
+        {
+            switch (score)
+            {
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                case 3:
+                    return "Forty";
+                default:
+                    return "Love";
+            }
+        }
+
+        private string GetScoreStatusWithinTieBreaker()
+        {
+            var minusResult = P1Score - P2Score;
+            if (IsPlayerOneAPointAhead()) return "Advantage player1";
+            if (IsPlayerOneAPointBehind()) return "Advantage player2";
+            if (IsPlayerOne2PointsAhead()) return "Win for player1";
+            return "Win for player2";
+        }
+
+
+        private bool IsPlayerOneAPointAhead()
+        {
+            return (P1Score - P2Score) == 1;
+        }
+
+        private bool IsPlayerOneAPointBehind()
+        {
+            return (P1Score - P2Score) == -1;
+        }
+        private bool IsPlayerOne2PointsAhead()
+        {
+            return (P1Score - P2Score) >= 2;
+        }
+
+
+        private bool IsPerformingTieBreaker()
+        {
+            return P1Score >= 4 || P2Score >= 4;
+        }
+
+        private string GetScoreStatusForEqualScores()
+        {
+            switch (P1Score)
+            {
+                case 0:
+                    return "Love-All";
+                case 1:
+                    return "Fifteen-All";
+                case 2:
+                    return "Thirty-All";
+                default:
+                    return "Deuce";
+            }
+        }
+
     }
 }
 
